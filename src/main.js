2b7252dde98de22fcb0918d9620127801899a745
Katherine Blaine <blaine.katherine12@gmail.com>
2023-01-11 17:01:53 -0600

    Add: selector and win count DOM element variables and update ul for difficult game type

--- src/main.js
+++ src/main.js
@@ -1,6 +1,12 @@
 var classicFighters = ['rock', 'paper', 'scissors'];
 var heroFighters = ['Wanda', 'Thor', 'Captain Marvel', 'Hulk', 'Spiderman'];
 
+// DOM elements:
+
+var classicSelector = document.getElementById('version-selector-classic');
+var difficultSelector = document.getElementById('version-selector-difficult');
+var compWinCount = document.getElementById('comp-win-counter');
+var humanWinCount = document.getElementById('human-win-counter');
 
 
 
@@ -10,20 +16,20 @@ var heroFighters = ['Wanda', 'Thor', 'Captain Marvel', 'Hulk', 'Spiderman'];
 
 // Test Game on Data Model code:
 // var game1 = new Game('classic')
-var game2 = new Game('difficult')
+// var game2 = new Game('difficult')
 
 // game1.createGameBoard('scissors')
-// console.log(this.gameBoard)
+// console.log(game1.gameBoard)
 // game1.playGame()
 // console.log('player1 fighter:', game1.playerOne.fighter)
 // console.log('player2 fighter:', game1.playerTwo.fighter)
 // console.log('player1 wins:' ,game1.playerOne.wins)
 // console.log('player2 wins:', game1.playerTwo.wins)
 
-game2.createGameBoard('Wanda')
-console.log(game2.gameBoard)
-game2.playGame()
-console.log('player1 fighter:', game2.playerOne.fighter)
-console.log('player2 fighter:', game2.playerTwo.fighter)
-console.log('player1 wins:' ,game2.playerOne.wins)
-console.log('player2 wins:', game2.playerTwo.wins)
+// game2.createGameBoard('Wanda')
+// console.log(game2.gameBoard)
+// game2.playGame()
+// console.log('player1 fighter:', game2.playerOne.fighter)
+// console.log('player2 fighter:', game2.playerTwo.fighter)
+// console.log('player1 wins:' ,game2.playerOne.wins)
+// console.log('player2 wins:', game2.playerTwo.wins)
