29f457b80e4601ef1cb30c82f931993aab7d679f
Katherine Blaine <blaine.katherine12@gmail.com>
2023-01-11 14:51:11 -0600

    Update: Delete comments and console logs from functional initial data model for classic game

--- src/game.js
+++ src/game.js
@@ -18,8 +18,6 @@ class Game {
     whoWon() {
         this.playerOne.takeTurn()
         this.playerTwo.takeTurn()
-        console.log(`player one fighter: ${this.playerOne.fighter}`)
-        console.log(`player two fighter: ${this.playerTwo.fighter}`)
         this.gameBoard = [this.playerOne.fighter, this.playerTwo.fighter]
         if (this.rules[`${this.gameBoard[0]} > ${this.gameBoard[1]}`]) {
             this.playerOne.wins += 1;
@@ -32,24 +30,3 @@ class Game {
         }
     }
 }
-
-// A new Game will be instantiated on page load. It will have a hard coded playerOne which will be the computer, and a hard coded playerTwo which will be the person.
-// ?? Only difference, as of now, and this may not be necessary, is that each page load will create a unique ID for both players.
-// An argument of 'name' will be passed through the name param and will differentiate between the computer and the human player.
-
-// This class needs a 'way to keep track of the data for the game board'
-// game board = the set of fighters displayed on the screen
-
-// This.winCombos = 
-
-// if playerOne.figter > playerTwo fighter 
-// playerOne.wins +=1;
-
-// Game Rules:
-// Rock beats scissors
-// scissors beats paper
-// paper beats rock
-
-// rock > scissors = true
-// scissors > paper = true
-// paper > rock = true
--- src/main.js
+++ src/main.js
@@ -1,7 +1,1 @@
 var fighters = ['rock', 'paper', 'scissors']
-
-var game1 = new Game('classic');
-
-game1.whoWon()
-console.log(`player one wins: ${game1.playerOne.wins}`)
-console.log(`player two wins: ${game1.playerTwo.wins}`)
