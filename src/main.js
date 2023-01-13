cc53ba693d1d007b961fcfba50af7de27409a0a5
Katherine Blaine <blaine.katherine12@gmail.com>
2023-01-13 16:07:01 -0600

    Update: refactor select game event listener and handler

--- src/main.js
+++ src/main.js
@@ -1,59 +1,50 @@
+// Data model:
 var classicFighters = ['rock', 'paper', 'scissors'];
 var heroFighters = ['Wanda', 'Thor', 'Captain Marvel', 'Hulk', 'Spiderman'];
 var currentGame;
 
 // DOM elements:
 
-// central game board
-
-var classicSelector = document.getElementById('classic-version-heading');
-var difficultSelector = document.getElementById('difficult-version-heading');
+// Home page elements -
 var gameTypeForm = document.getElementById('game-type-form')
 var gameBoardHeading = document.getElementById('game-board-heading')
 var gameBoard = document.getElementById('game-board')
-
 var versionSelector = document.getElementById('game-type-form')
 
-// classic game board
+// Game board elements -
 var classicGameBoard = document.getElementById('classic-game-board')
-
-// difficult
 var difficultGameBoard = document.getElementById('difficult-game-board')
-
 var navButtons = document.getElementById('nav')
 var changeGameButton = document.getElementById('change-game-button')
 
-// Sidebar Elements:
+// Sidebar Elements -
 var compWinCount = document.getElementById('comp-win-counter');
 var humanWinCount = document.getElementById('human-win-counter');
 
 // Event listeners:
-
-classicSelector.addEventListener('click', selectClassic)
-difficultSelector.addEventListener('click', selectDifficult)
+gameTypeForm.addEventListener('click', function() {
+    selectGame(event)})
 changeGameButton.addEventListener('click', changeGameMode)
 
 // Event handlers:
-
-function selectClassic() {
-    hide(gameTypeForm)
-    show(classicGameBoard)
-    show(navButtons)
-    currentGame = 'classic'
+function selectGame(event) {
+        if (event.target.parentNode.className === 'version-selector-classic') {
+            currentGame = new Game('classic');
+            hide(gameTypeForm)
+            show(classicGameBoard)
+            show(navButtons)
+        } else if (event.target.parentNode.className === 'version-selector-difficult'){
+            currentGame = new Game('difficult');
+            hide(gameTypeForm)
+            show(difficultGameBoard)
+            show(navButtons)
+    }
     gameBoardHeading.innerText = 'Choose your fighter'
-};
-
-function selectDifficult() {
-    hide(gameTypeForm)
-    show(difficultGameBoard)
-    show(navButtons)
-    currentGame = 'difficult'
-    gameBoardHeading.innerText = 'Choose your fighter'
-};
+}
 
 function changeGameMode() {
-    if (currentGame === 'classic') {
-        console.log('hello')
+   var currentGameMode = currentGame.gameType
+    if (currentGameMode === 'classic') {
         show(gameTypeForm)
         hide(classicGameBoard)
     } else {
@@ -61,8 +52,6 @@ function changeGameMode() {
         hide(difficultGameBoard)
     }
 }
-
-
 
 // Hide and Show functions:
 
@@ -73,24 +62,3 @@ function show(element) {
   function hide(element) {
     element.classList.add("hidden");
   };
-
-
-// Test Game on Data Model code:
-// var game1 = new Game('classic')
-// var game2 = new Game('difficult')
-
-// game1.createGameBoard('scissors')
-// console.log(game1.gameBoard)
-// game1.playGame()
-// console.log('player1 fighter:', game1.playerOne.fighter)
-// console.log('player2 fighter:', game1.playerTwo.fighter)
-// console.log('player1 wins:' ,game1.playerOne.wins)
-// console.log('player2 wins:', game1.playerTwo.wins)
-
-// game2.createGameBoard('Wanda')
-// console.log(game2.gameBoard)
-// game2.playGame()
-// console.log('player1 fighter:', game2.playerOne.fighter)
-// console.log('player2 fighter:', game2.playerTwo.fighter)
-// console.log('player1 wins:' ,game2.playerOne.wins)
-// console.log('player2 wins:', game2.playerTwo.wins)
